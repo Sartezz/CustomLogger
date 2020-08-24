7d0da9f6b1006011b361e7ab4a9517c923a0be22
Igor Levchenko <i.levchenko@softteco.com>
2020-08-24 19:20:52 +0300

    Initial commit

--- sample/src/main/java/com/example/testlib/MainActivity.kt
+++ sample/src/main/java/com/example/testlib/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.testlib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testlibu.CustomLogger
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
